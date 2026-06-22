bb1674ede12e2eba6576be4512079d884fb2e95d
anishkr10 <anishmusic.management@gmail.com>
2026-06-22 10:25:13 +0530

    Variable

---
+++ 02_Chapter2/02_Variables.py
@@ -0,0 +1,21 @@
+#Variables
+
+a = 12
+print(a)
+
+anish = 56
+print(anish)
+
+# Anyting can be a variable , but there are some rules for naming a variable
+
+    # 1. A variable name must start with a letter or the underscore character
+
+    # 2. A variable name cannot start with a number
+        # 1a = 56 ---> This is not a valid variable name because it starts with a number
+
+    # 3. A variable name cannot contain any spaces
+         # anish kumar = 56 ---> This is not a valid variable name because it contains spaces
+
+    # 4. A variable name cannot contain any special characters (except underscore)
+        # anish@kumar = 56 ---> This is not a valid variable name because it contains special character
+        #Special characters are like @, #, $, %, ^, &, *, (, ), -, +, =, {, }, [, ], |, \, :, ;, ", ', <, >, ,, ., ?, /, ~, `
