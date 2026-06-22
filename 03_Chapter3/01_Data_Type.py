1cfbb33c34b9232a1cf318d5a4a9ae3337c6386a
anishkr10 <anishmusic.management@gmail.com>
2026-06-22 23:52:31 +0530

    Data Type

---
+++ 03_Chapter3/01_Data_Type.py
@@ -0,0 +1,49 @@
+#Integer Data Type (int)
+a = -12
+b = 56
+c = 45
+
+#int -> 0 to infinity and -1 to -infinity
+print(type(a))
+print(type(b))
+print(type(c))
+
+#Float Data Type (float)
+# Float are the values which include decimal & fraction
+d = 12.5
+print(type(d))
+
+e = 5/2
+print(type(e))
+
+#Complex Data Type (j)
+f = 12+ 3j
+print (type(f))
+
+# String Data Type ("")
+name = "Anish Kumar"
+print(type(name))
+
+#Single & double quotes uses
+
+message = 'hii how are you"i am fine"'
+print(message)
+print(type(message))
+
+
+#Boolean Data Type
+# Boolean data type is used to represent the truth values (True or False)
+
+g = True
+h = False
+print(type(g))
+print(type(h))
+
+i = "True" #This is a string not a boolean
+print(type(i))
+
+#None Data Type
+# Represent noting : None
+
+j = None
+print(type(j))
