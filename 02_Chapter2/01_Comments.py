ab6a20ed898d363ec470a2183957619fe58c2ddb
anishkr10 <anishmusic.management@gmail.com>
2026-06-22 10:11:40 +0530

    Add Multiline Comments

--- 02_Chapter2/01_Comments.py
+++ 02_Chapter2/01_Comments.py
@@ -1,3 +1,8 @@
 # This line below is used for printing ----> This is a single line comment
 print("Hello Everyone!")
 
+"""
+Hello How are you
+This is a multi line comment
+This is used for writing multiple lines of comments
+"""
