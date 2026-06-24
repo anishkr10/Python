639c42bff56dc7285554b0007f54926c2fd57fb1
anishkr10 <anishmusic.management@gmail.com>
2026-06-24 10:31:57 +0530

    String - Unicode

---
+++ 04_Chapter4/01_String.py
@@ -0,0 +1,17 @@
+a = "Anish" # Anish is a string
+
+b = "H"
+print(ord(b))
+#ord() function returns the unicode code point for a one-character string
+
+c = "3"
+print(ord(c))
+
+d = "%"
+print(ord(d))
+
+e = "How are you?"
+print(ord(e[8])) #ord() function returns the unicode code point for a one-character string
+
+f = "H"
+print(ord(f)) #ord() function returns the unicode code point for a one-character string
