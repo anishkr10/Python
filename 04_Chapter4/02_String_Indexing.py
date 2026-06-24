5b42671a247b455fd9e1ab41f8b331563678dc3f
anishkr10 <anishmusic.management@gmail.com>
2026-06-24 10:52:30 +0530

    String Indexing

---
+++ 04_Chapter4/02_String_Indexing.py
@@ -0,0 +1,19 @@
+#Positive Indexing
+a = "COLLEGE"
+#print(a[0]) # Output: C
+
+print(a[2]) # Output: L
+
+
+
+#Negative Indexing
+b = "ANISH"
+
+print(b[-1]) # Output: H
+print(b[-4]) # Output: N
+
+
+
+#Checking both positive and negative indexing
+c = "PYTHON"
+print(c[2], c[-3]) # Output: T H
