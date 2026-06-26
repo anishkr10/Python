52f37afa6ba1e8c19114dfec81622a56e638cd3a
anishkr10 <anishmusic.management@gmail.com>
2026-06-26 23:08:05 +0530

    Input Data Type

--- 05_Chapter5/02_input.py
+++ 05_Chapter5/02_input.py
@@ -1,7 +1,14 @@
-age = input("What is your age : ")
+# age = input("What is your age : ")
 
 # print(age)
 
 # using formatted string
+
+# print(f"Your age is {age}") 
+
+
+# Data type of an input
+
+age = int(input("What is your age : "))
 
 print(f"Your age is {age}") 
