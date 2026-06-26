ca9da97bc0286a8aced17f2b64eb1445e8e31ebc
anishkr10 <anishmusic.management@gmail.com>
2026-06-26 11:18:19 +0530

    Type Conversion

---
+++ 04_Chapter4/05_type_conversion.py
@@ -0,0 +1,22 @@
+# Type Conversion : Type conversion is the process of changing one data type into another data type.
+
+a = "12"
+b = int (a)
+
+print(type(a))
+print(type(b))
+
+
+# you can convert strings if its holds valid integer
+
+# c = "12.5"
+# c = int(c)
+
+# print(c)
+
+# convert sting to float
+
+d = "12.4"
+d = float(d)
+
+print(d)
