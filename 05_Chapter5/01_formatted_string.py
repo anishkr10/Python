2bd4b6e297fafd243e2b758591d45c816a0d2922
anishkr10 <anishmusic.management@gmail.com>
2026-06-26 22:39:13 +0530

    Formatted String

--- 05_Chapter5/01_formatted_string.py
+++ 05_Chapter5/01_formatted_string.py
@@ -1,5 +1,10 @@
-# Formated Strings
+# Formatted Strings
 
 name = "Anish"
 age = 21
 print(f"Hii My name is {name} and my age is {age}") 
+
+# Using Multiple data types in formatted strings
+
+print("Hii My name is",name,"and my age is",age) 
+
