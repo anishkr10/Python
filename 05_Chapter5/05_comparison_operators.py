e46e97af01b2a40dc892b03e04727b64e84046f5
anishkr10 <anishmusic.management@gmail.com>
2026-06-29 22:40:35 +0530

    Comparison Operators

---
+++ 05_Chapter5/05_comparison_operators.py
@@ -0,0 +1,28 @@
+# Equal to Operators (==)
+
+print(12==12) #Output: True
+print(14==12) #Output: False
+
+# Not Equal to
+
+print(5!=7) #output: True
+
+
+# Greater than  (>)
+
+print(15>5) #Output: True
+
+
+# Less than  (<)
+
+print(15<5) #Output: False
+
+
+# greater than or equal to (>=)
+
+print(15>=4) #Output: True
+
+
+# Smaller than or equal to (<=)
+
+print(15<=4) #Output: False
