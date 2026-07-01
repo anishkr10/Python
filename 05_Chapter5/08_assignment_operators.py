a291f975ab28d1712f4f1817026ba59859cb5882
anishkr10 <anishmusic.management@gmail.com>
2026-07-01 10:29:29 +0530

    Assignment Operators

---
+++ 05_Chapter5/08_assignment_operators.py
@@ -0,0 +1,69 @@
+# Assignment Operators
+
+# = operator
+
+a = 12
+
+a = a + 10
+a = a + 10
+a = a + 10
+
+print(a)
+
+
+# += operator
+
+b = 15
+
+b += 10
+b += 10
+print(b)
+
+# -= operator
+
+c = 20
+
+c -= 10
+c -= 10
+print(c)
+
+# *= operator
+
+d = 5
+
+d *= 10
+d *= 10
+print(d)
+
+# /= operator
+
+e = 100
+
+e /= 10
+e /= 10
+print(e)
+
+# %= operator
+
+f = 100
+
+f %= 10
+f %= 10
+print(f)
+
+# **= operator
+
+g = 2
+
+g **= 3
+g **= 2
+print(g)
+
+# //= operator
+
+h = 100
+
+h //= 10
+h //= 10
+
+print(h)
