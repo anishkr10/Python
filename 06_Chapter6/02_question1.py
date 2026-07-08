3397a7e3d9d4a61d6cd40eabb704926caabdd261
anishkr10 <anishmusic.management@gmail.com>
2026-07-08 11:02:29 +0530

    if-else condition

--- 06_Chapter6/02_question1.py
+++ 06_Chapter6/02_question1.py
@@ -1,1 +1,8 @@
-# 
+# You have to take an input of age and tell the person whether they are eligible to vote or not. The voting age is 18 years.
+
+age = int(input("Enter Your Age: ")) 
+
+if age >= 18:
+    print("You are eligible for vote.")
+else:
+    print("You are not eligible for vote.")
