b72c1e07074bf935d06af500c08b4cc9ba4469b2
anishkr10 <anishmusic.management@gmail.com>
2026-07-08 10:40:48 +0530

    If Condition

---
+++ 06_Chapter6/01_control_flow.py
@@ -0,0 +1,14 @@
+if True:
+    print("hello how are you") # Output: hello how are you
+
+if False:
+    print("hello how are you") # Output: (No output, as the condition is False)
+
+if 12==12:
+    print("hello how are you") # Output: hello how are you
+
+if 30 !=20:
+    print("hello how are you") # Output: hello how are you
+
+if 12!=12:
+    print("hello how are you") # Output: (No output, as the condition is False)
